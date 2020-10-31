1e60439d439cb26ca470b8b61b394f77f0887e47
Shradha <rajan.bhatia@hotmail.com>
2020-10-31 16:02:42 +0530

    scynronous ball

--- sketch.js
+++ sketch.js
@@ -1,13 +1,26 @@
 var ball;
-
+var database,position;
 function setup(){
     createCanvas(500,500);
+    database=firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+var ballPositionRef=database.ref("ball/position");
+ballPositionRef.on("value",readPosition,showError)
+
+}
+function readPosition(data){
+    position=data.val();
+    ball.x=position.x
+    ball.y=position.y
 }
 
+function showError(){
+   console.log("error") 
+}
 function draw(){
     background("white");
+    if(position!==undefined ){
     if(keyDown(LEFT_ARROW)){
         changePosition(-1,0);
     }
@@ -20,10 +33,15 @@ function draw(){
     else if(keyDown(DOWN_ARROW)){
         changePosition(0,+1);
     }
+}
     drawSprites();
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+  database.ref("ball/position").set({ 
+
+    x:position.x+x,y:position.y+y
+  });
+
 }
+
